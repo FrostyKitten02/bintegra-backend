77de5d0d3525e08fd9827569360ab2dae12b54e0
FrostyKitten02 <alen.fridau02@gmail.com>
2023-06-06 11:52:32 +0200

    to consultat presmission check

--- modules/Headless-rest/src/main/java/si/bintegra/sp/application/ConsultantController.java
+++ modules/Headless-rest/src/main/java/si/bintegra/sp/application/ConsultantController.java
@@ -1,17 +1,22 @@
 package si.bintegra.sp.application;
 
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.PortalUtil;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ServiceScope;
 import si.bintegra.sp.dto.request.ConsultantRequest;
 import si.bintegra.sp.service.ConsultantLocalServiceUtil;
+import si.bintegra.sp.util.RoleChecker;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
 
 @Component(
         properties = "defaultController.properties",
@@ -24,10 +29,12 @@ import javax.ws.rs.core.Application;
 public class ConsultantController extends Application {
 
     @POST
-    public Object addConsultant(@RequestBody ConsultantRequest req) {
+    public Object addConsultant(@RequestBody ConsultantRequest req, @Context HttpServletRequest request) throws PortalException {
         if (req.getUserId() == null) {
-            return new Object();
+            throw new IllegalArgumentException("userIdIsRequired");
         }
+
+        RoleChecker.isUserAdministratorStrict(PortalUtil.getUser(request));
 
         ConsultantLocalServiceUtil.addConsultant(req.getUserId());
         return new Object();
